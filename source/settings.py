bdeec68cdbd547fffe8b84e29023ddbe0ffa3d88
kylegraupe <91435626+kylegraupe@users.noreply.github.com>
2024-12-19 15:02:31 -0500

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -12,7 +12,6 @@ def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
     try:
-        # doesn't even have to be reachable
         s.connect(('10.254.254.254', 1))
         IP = s.getsockname()[0]
     except Exception:
@@ -31,7 +30,6 @@ UI_ON = False
 # RTMP/NGINX settings
 LISTENING_PORT=1935
 ip_address = get_ip()
-# ip_address = '172.20.10.2' # Hotspot
 RTMP_URL=f'rtmp://{ip_address}:{LISTENING_PORT}/live/'
 
 # Model properties
